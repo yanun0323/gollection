87f80e3d205625d66a2c30be9cb345715a104f08
Yanun Yang <a1256212562@gmail.com>
2022-03-06 04:22:37 +0800

    Add interface

---
+++ interface.go
@@ -0,0 +1,43 @@
+package gollection
+
+type T interface{}
+
+type IQueue interface {
+	Clear()
+	Clone() IQueue
+	Contains(T) bool
+	ContainsAny(...T) bool
+	Dequeue() T
+	Enqueue(T)
+	IsEmpty() bool
+	Peek() T
+	ToArray() []T
+}
+
+type IStack interface {
+	Clear()
+	Clone() IStack
+	Contains(T) bool
+	ContainsAny(...T) bool
+	IsEmpty() bool
+	Peek() T
+	Pop() T
+	Push(T)
+	ToArray() []T
+}
+
+type IList interface {
+	ADD(...T)
+	At(int) T
+	Clear()
+	Clone() IList
+	Contains(T) bool
+	ContainsAny(...T) bool
+	Insert(int, ...T) bool
+	IsEmpty() bool
+	Remove(T) bool
+	RemoveAll(T) bool
+	RemoveAt(int) bool
+	Set(int, T) bool
+	ToArray() []T
+}
